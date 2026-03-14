d948ad917c30b785b09f2fc92a6eb3d68fbf1061
Matheus Banhos <matheusbanhos19@hotmail.com>
2026-03-14 18:29:20 -0300

    Feature: Create business rules for user registration.

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,37 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.Services.AutoMapper;
+// using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace MyRecipeBook.Application;
+
+public static class DependencyInjectionExtension
+{
+    public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddAutoMapper(services);
+        AddUserCases(services);
+        // AddUPasswordEncrypter(services, configuration);
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        services.AddScoped(options => new AutoMapper.MapperConfiguration(options =>
+        {
+            options.AddProfile(new AutoMapping());
+        }).CreateMapper());
+    }
+
+    private static void AddUserCases(IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+    }
+    
+    // private static void AddUPasswordEncrypter(IServiceCollection services, IConfiguration configuration)
+    // {
+    //     var addionalKey = configuration.GetValue<string>("Settings:Password:AdditionalKey");
+    //     
+    //     services.AddScoped(option => new PasswordEncripter(addionalKey!));
+    // }
+}
